431ae9c39c61af1f51f40457ccd4787e8fd8039f
@yas <yas@biba.uni-bremen.de>
2022-12-02 16:45:33 +0100

    feature map and acc and loss plot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # import the necessary packages
+from keras.applications.vgg16 import preprocess_input
 from keras.applications import VGG16
 from keras.layers import Flatten
 from keras.layers import Dense
@@ -13,27 +14,30 @@ from keras.utils import to_categorical
 from sklearn.preprocessing import LabelBinarizer
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import expand_dims
+import warnings
+warnings.filterwarnings('ignore')
 import cv2
 import os
+import pickle
 # define the base path to the input dataset and then use it to derive
 # the path to the images directory and annotation CSV file
 BASE_PATH = "dataset"
 IMAGES_PATH = os.path.sep.join([BASE_PATH, "JPEGImages"])
 ANNOTS_PATH = os.path.sep.join([BASE_PATH, "annotations.csv"])
-
 # define the path to the base output directory
 BASE_OUTPUT = "output"
 # define the path to the output serialized model, model training plot,
 # and testing image filenames
 MODEL_PATH = os.path.sep.join([BASE_OUTPUT, "detector.h5"])
-PLOT_PATH = os.path.sep.join([BASE_OUTPUT, "plot.png"])
+LOSS_PATH = os.path.sep.join([BASE_OUTPUT, "loss.png"])
+ACCURACY_PATH = os.path.sep.join([BASE_OUTPUT, "acc.png"])
 TEST_FILENAMES = os.path.sep.join([BASE_OUTPUT, "test_images.txt"])
-
-
+LB_PATH = os.path.sep.join([BASE_OUTPUT, "lb.pickle"])
 # initialize our initial learning rate, number of epochs to train
 # for, and the batch size
 INIT_LR = 1e-4
-NUM_EPOCHS = 20
+NUM_EPOCHS = 1
 BATCH_SIZE = 32
 # load the contents of the CSV annotations file
 print("[INFO] loading dataset...")
@@ -118,7 +122,6 @@ f = open(TEST_FILENAMES, "w")
 print(TEST_FILENAMES)
 f.write("\n".join(TEST_FILENAMES))
 f.close()
-
 # load the VGG16 network, ensuring the head FC layers are left off
 vgg = VGG16(weights="imagenet", include_top=False,
 	input_tensor=Input(shape=(224, 224, 3)))
@@ -137,51 +140,22 @@ bboxHead = Dense(4, activation="sigmoid",
 	name="bounding_box")(bboxHead)
 # construct a second fully-connected layer head, this one to predict
 # the class label
-# softmaxHead = Dense(512, activation="relu")(flatten)
-# softmaxHead = Dropout(0.5)(softmaxHead)
-# softmaxHead = Dense(512, activation="relu")(softmaxHead)
-# softmaxHead = Dropout(0.5)(softmaxHead)
-# softmaxHead = Dense(len(lb.classes_), activation="sigmoid",
-# 	name="class_label")(softmaxHead)
+softmaxHead = Dense(512, activation="relu")(flatten)
+softmaxHead = Dropout(0.5)(softmaxHead)
+softmaxHead = Dense(512, activation="relu")(softmaxHead)
+softmaxHead = Dropout(0.5)(softmaxHead)
+softmaxHead = Dense(len(lb.classes_), activation="sigmoid",
+	name="class_label")(softmaxHead)
 # put together our model which accept an input image and then output
 # bounding box coordinates and a class label
 model = Model(
 	inputs=vgg.input,
 	outputs=(bboxHead))
-# define a dictionary to set the loss methods -- categorical
-# cross-entropy for the class label head and mean absolute error
-# for the bounding box head
-# losses = {
-# 	"class_label": "binary_crossentropy",
-# 	"bounding_box": "mean_squared_error",
-# }
-#
-# # define a dictionary that specifies the weights per loss (both the
-# # class label and bounding box outputs will receive equal weight)
-# lossWeights = {
-# 	"class_label": 1.0,
-# 	"bounding_box": 1.0
-# }
-#
 # initialize the optimizer, compile the model, and show the model
 # summary
 opt = Adam(lr=INIT_LR)
 model.compile(loss="mse", optimizer=opt, metrics=["accuracy"])
 print(model.summary())
-
-# # construct a dictionary for our target training outputs
-# trainTargets = {
-# 	"class_names": trainFilenames,
-# 	"bounding_box": trainBBoxes
-# }
-#
-# # construct a second dictionary, this one for our target testing
-# # outputs
-# testTargets = {
-# 	"class_name": testFilenames,
-# 	"bounding_box": testBBoxes
-# }
-# train the network for bounding box regression and class label
 # prediction
 print("[INFO] training model...")
 print("[INFO] training bounding box regressor...")
@@ -195,47 +169,57 @@ H = model.fit(
 # serialize the model to disk
 print("[INFO] saving object detector model...")
 model.save(MODEL_PATH, save_format="h5")
+for i in range(len(model.layers)):
+    layer = model.layers[i]
+    if 'conv' not in layer.name:
+        continue
+    print(i , layer.name , layer.output.shape)
+model = Model(inputs=model.inputs, outputs=model.layers[1].output)
+image = load_img( image , target_size=(224,224))
+# expand dimensions so that it represents a single 'sample'
+image = expand_dims(image, axis=0)
+# prepare the image (e.g. scale pixel values for the vgg)
+image = preprocess_input(image)
+# get feature map for first hidden layer
+feature_maps = model.predict(image)
+fig = plt.figure(figsize=(20, 15))
+for i in range(1, feature_maps.shape[3] + 1):
+    plt.subplot(8, 8, i)
+    plt.imshow(feature_maps[0, :, :, i - 1], cmap='gray')
 
-# # serialize the label binarizer to disk
-# print("[INFO] saving label binarizer...")
-# f = open(config.LB_PATH, "wb")
-# f.write(pickle.dumps(lb))
-# f.close()
+plt.show()
+# serialize the label binarizer to disk
+print("[INFO] saving label binarizer...")
+f = open(LB_PATH, "wb")
+f.write(pickle.dumps(lb))
+f.close()
 # plot the total loss, label loss, and bounding box loss
-lossNames = ["loss", "class_label_loss", "bounding_box_loss"]
+lossNames = ["loss"]
 N = np.arange(0, NUM_EPOCHS)
 plt.style.use("ggplot")
 (fig, ax) = plt.subplots(3, 1, figsize=(13, 13))
-
-# loop over the loss names
-for (i, l) in enumerate(lossNames):
-	# plot the loss for both the training and validation data
-	title = "Loss for {}".format(l) if l != "loss" else "Total loss"
-	ax[i].set_title(title)
-	ax[i].set_xlabel("Epoch #")
-	ax[i].set_ylabel("Loss")
-	ax[i].plot(N, H.history[l], label=l)
-	ax[i].plot(N, H.history["val_" + l], label="val_" + l)
-	ax[i].legend()
-
-# save the losses figure and create a new figure for the accuracies
-plt.tight_layout()
-plotPath = os.path.sep.join([PLOT_PATH, "losses.png"])
-plt.savefig(plotPath)
-plt.close()
-# create a new figure for the accuracies
+ #create a new figure for the accuracies
+N = NUM_EPOCHS
 plt.style.use("ggplot")
 plt.figure()
-plt.plot(N, H.history["class_label_accuracy"],
-	label="class_label_train_acc")
-plt.plot(N, H.history["val_class_label_accuracy"],
-	label="val_class_label_acc")
-plt.title("Class Label Accuracy")
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.title("Bounding Box Regression Loss on Training Set")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss")
+plt.legend(loc="lower left")
+plt.savefig(LOSS_PATH)
+plt.close()
+# #create a new figure for the accuracies
+N = NUM_EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["accuracy"], label="Accuracy")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_Accuracy")
+plt.title("Bounding Box Regression Loss on Training Set")
 plt.xlabel("Epoch #")
 plt.ylabel("Accuracy")
 plt.legend(loc="lower left")
-
-# save the accuracies plot
-plotPath = os.path.sep.join([PLOT_PATH, "accs.png"])
-plt.savefig(plotPath)
+plt.savefig(ACCURACY_PATH)
+plt.close()
 
